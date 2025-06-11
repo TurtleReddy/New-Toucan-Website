43e879a9b16563a17fcb8377a335b1084164a07f
TurtleReddy <144063277+TurtleReddy@users.noreply.github.com>
2025-06-10 23:13:09 -0700

    Minor Changes
    Added Math page
    Reworded home page

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
-tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+const hasSeenIntro = sessionStorage.getItem("seenIntro");
+if (!hasSeenIntro) {
+  const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+  tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+  tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+  tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+  tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+  tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+  sessionStorage.setItem("seenIntro", "true");
+} else {
+  document.querySelector(".slider").style.transform = "translateY(-100%)";
+  document.querySelector(".intro").style.transform = "translateY(-100%)";
+  document.querySelector("nav").style.opacity = "1";
+  document.querySelector(".big-text").style.opacity = "1";
+}
